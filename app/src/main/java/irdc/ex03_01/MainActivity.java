32c6909810551c1bf69535fdb90f3618e2549505
Koyama-Tsubasa <a0918050152@gmail.com>
2019-04-11 10:54:23 +0800

    ex09_01 new

--- app/src/main/java/irdc/ex03_01/MainActivity.java
+++ app/src/main/java/irdc/ex03_01/MainActivity.java
@@ -2,17 +2,25 @@ package irdc.ex03_01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextView01,mTextView02;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView01 = (TextView) findViewById(R.id.myTextView01);
+    }
+
+    public void Next(View view)
+    {
+        mTextView01.setY(mTextView01.getY() - 200);
         mTextView02 = (TextView) findViewById(R.id.myTextView02);
-        String str_2 = "test 測試 Android TextView";
+        String str_2 = "It's a temple used in the movie ' 君の名は '  ";
         mTextView02.setText(str_2);
     }
+
 }
